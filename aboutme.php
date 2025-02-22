e3e36dff1d225b17f6493cc7c421cd46261e88b8
Eksansilus <eksansilus@gmail.com>
2025-02-23 01:45:34 +0700

    Update aboutme.php

--- aboutme.php
+++ aboutme.php
@@ -1,5 +1,5 @@
 <?php
-$aboutme = 'I am a Manual Quality Assurance Tester with recent training from Growia Education.
+$aboutme = "I am a Manual Quality Assurance Tester with recent training from Growia Education.
             I am passionate about building a career in Quality Assurance (QA) Testing.
             My course experience has provided me with essential software testing skills,
             including creating test cases, writing test plans, and conducting manual testing to ensure software quality.
@@ -7,5 +7,5 @@ $aboutme = 'I am a Manual Quality Assurance Tester with recent training from Gro
             I have strong analytical abilities, attention to detail,
             and an aptitude for team collaboration to identify and report software issues.
             My technical background offers me a solid foundation in logical reasoning,
-            which aids in understanding and testing applications effectively.';
+            which aids in understanding and testing applications effectively.";
 ?>
